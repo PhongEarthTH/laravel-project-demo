d875735c976f0a89982198a08eb08ce2cfe94461
poppolathth <102400389+PhongEarthTH@users.noreply.github.com>
2025-06-16 16:09:58 +0700

    fix: Add comment in CKEditor initialization for registration link

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -31,6 +31,7 @@
             .create(document.querySelector('#content'), {
                 licenseKey: 'eyJhbGciOiJFUzI1NiJ9.eyJleHAiOjE3NTAyMDQ3OTksImp0aSI6IjFlMDljOTU2LWI0MGEtNDU1NS1iODcyLTczMDJjZWRjNTZiNCIsImxpY2Vuc2VkSG9zdHMiOlsiKi53ZWJjb250YWluZXIuaW8iLCIqLmpzaGVsbC5uZXQiLCIqLmNzcC5hcHAiLCJjZHBuLmlvIiwiMTI3LjAuMC4xIiwibG9jYWxob3N0IiwiMTkyLjE2OC4qLioiLCIxMC4qLiouKiIsIjE3Mi4qLiouKiIsIioudGVzdCIsIioubG9jYWxob3N0IiwiKi5sb2NhbCJdLCJkaXN0cmlidXRpb25DaGFubmVsIjpbImNsb3VkIiwiZHJ1cGFsIiwic2giXSwibGljZW5zZVR5cGUiOiJldmFsdWF0aW9uIiwidmMiOiIzZGMwNDQ5OSJ9.azv4SjqGgR6CP - Z116OYKzPlQK0tufUALlqg1SNVdqZvOPwa_TpCPtbsr2HKCQ6yPNDdCOIghUumClGEGfnjYQ',
                 // Create a free account on https://portal.ckeditor.com/checkout?plan=free
+                //ลิงค์สมัคร
             })
             .catch(error => {
                 console.error(error);
